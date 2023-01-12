7d35dc2965388e8351570ff38bb582e1c286962f
OPaslavskiy <bankir.op@gmail.com>
2023-01-12 14:20:45 +0200

    fetch -OK, markUP - OK

--- src/index.js
+++ src/index.js
@@ -1,18 +1,62 @@
+const axios = require('axios').default;
+
 const KEY_API_PIXABAY = '32781851-5380a4cc45169f3ca42a551c9';
+const URL_PIXABAY = `https://pixabay.com/api/`;
+const currentPage = 1;
+
+const divGallery = document.querySelector('.gallery');
 
 const form = document.querySelector('.search-form');
 
-form.addEventListener('submit', sendRequest);
+form.addEventListener('submit', searchUser);
 
-function sendRequest(event) {
+function searchUser(event) {
   event.preventDefault();
   const userRequest = event.currentTarget.elements.searchQuery.value;
   console.log('123');
+  fetchRequest(userRequest).then(card => createMarkup(card.data.hits));
+}
 
-  fetch(
-    `https://pixabay.com/api/?key=${KEY_API_PIXABAY}&q=${userRequest}&image_type=photo&orientation=horizontal&safesearch=true`
-  )
-    .then(response => response.json())
-    .then(posts => console.log(posts))
-    .catch(error => console.log(error));
+async function fetchRequest(userRequest) {
+  try {
+    const response = await axios.get(
+      `${URL_PIXABAY}?key=${KEY_API_PIXABAY}&q=${userRequest}&image_type=photo&orientation=horizontal&safesearch=true&per_page=40&page=${currentPage}`
+    );
+    return response;
+  } catch (error) {
+    console.log(error);
+  }
+}
+
+function createMarkup(arrayPhoto) {
+  const markup = arrayPhoto
+    .map(
+      ({
+        webformatURL,
+        largeImageURL,
+        tags,
+        likes,
+        views,
+        comments,
+        downloads,
+      }) => `<div class="photo-card">
+    <img src="${webformatURL}" alt="${tags}" loading="lazy" />
+    <div class="info">
+      <p class="info-item">
+        <b>Likes: ${likes}</b>
+      </p>
+      <p class="info-item">
+        <b>Views: ${views}</b>
+      </p>
+      <p class="info-item">
+        <b>Comments: ${comments}</b>
+      </p>
+      <p class="info-item">
+        <b>Downloads: ${downloads}</b>
+      </p>
+    </div>
+  </div>`
+    )
+    .join('');
+  divGallery.insertAdjacentHTML('afterbegin', markup);
 }
